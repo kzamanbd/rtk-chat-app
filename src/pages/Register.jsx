9504fa9c6eb376eb9cf7c003469375014acc92d5
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-06-20 09:25:12 +0600

    update registration page ui

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,9 @@
+import ApplicationLogo from '@/components/shared/ApplicationLogo';
+import LoadingSpinner from '@/components/shared/LoadingSpinner';
+import OtherLoginOption from '@/components/shared/OtherLoginOption';
 import { useRegisterMutation } from '@/features/auth/authApi';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Register() {
 	const navigate = useNavigate();
@@ -8,9 +11,10 @@ export default function Register() {
 
 	// local state
 	const [fullName, setFullName] = useState('');
-	const [email, setEmail] = useState('');
+	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState('');
+	const [registerError, setRegisterError] = useState('');
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
@@ -19,137 +23,111 @@ export default function Register() {
 			return;
 		}
 		try {
-			await register({ name: fullName, email, password, withLogin: true }).unwrap();
+			await register({ name: fullName, email: username, password, withLogin: true }).unwrap();
 			navigate('/');
 		} catch (error) {
 			console.log(error);
-			alert(error.data?.message);
+			setRegisterError(error.data.message);
 		}
 	};
 
 	return (
-		<div className="grid place-items-center h-screen bg-[#F9FAFB">
-			<div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-				<div className="max-w-md w-full space-y-8">
-					<div>
-						<img className="mx-auto h-12 w-auto" src="/logo.svg" alt="logo" />
-						<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create your account</h2>
-					</div>
-					<form className="mt-8 space-y-6" onSubmit={handleSubmit} method="POST">
-						<input type="hidden" name="remember" value="true" />
-						<div className="rounded-md shadow-sm -space-y-px">
-							<div>
-								<label htmlFor="name" className="sr-only">
-									Full Name
-								</label>
+		<div className="bg-light-gray dark:bg-dark-secondary dark:text-gray-300">
+			<div className="flex min-h-screen items-center justify-center p-6">
+				<div className="login-bg">
+					<div className="rounded-md bg-white p-8 shadow-md">
+						<div className="my-6 flex items-center justify-center space-x-2">
+							<span className="h-12 w-12" alt="logo">
+								<ApplicationLogo />
+							</span>
+							<span className="dark--text text-3xl font-semibold">RTK Chat</span>
+						</div>
+						<div className="my-3">
+							<p className="dark--text my-2 text-2xl font-semibold">Welcome to RTK Chat App!</p>
+							<p className="text-xs text-gray-600">
+								Please sign-up to your account and start the adventure
+							</p>
+						</div>
+
+						<OtherLoginOption />
+
+						<form className="mt-4" onSubmit={handleSubmit}>
+							{registerError && <div className="text-red-500 text-center">{registerError}</div>}
+							<label className="block">
+								<span className="form-label">Email</span>
 								<input
-									id="name"
-									name="Name"
-									type="Name"
-									autoComplete="Name"
-									required
-									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-									placeholder="Name"
+									type="text"
+									name="fullName"
+									className="form-input"
+									placeholder="Email Address"
 									value={fullName}
 									onChange={(e) => setFullName(e.target.value)}
 								/>
-							</div>
+							</label>
 
-							<div>
-								<label htmlFor="email-address" className="sr-only">
-									Email address
-								</label>
+							<label className="block mt-3">
+								<span className="form-label">Email</span>
 								<input
-									id="email-address"
-									name="email"
 									type="email"
-									autoComplete="email"
-									required
-									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-									placeholder="Email address"
-									value={email}
-									onChange={(e) => setEmail(e.target.value)}
+									name="email"
+									className="form-input"
+									placeholder="Email Address"
+									value={username}
+									onChange={(e) => setUsername(e.target.value)}
 								/>
-							</div>
+							</label>
 
-							<div>
-								<label htmlFor="password" className="sr-only">
-									Password
-								</label>
+							<label className="mt-3 block">
+								<span className="form-label">Password</span>
 								<input
-									id="password"
-									name="password"
 									type="password"
-									autoComplete="current-password"
-									required
-									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-									placeholder="Password"
+									name="password"
+									className="form-input"
+									placeholder="********"
 									value={password}
 									onChange={(e) => setPassword(e.target.value)}
 								/>
-							</div>
+							</label>
 
-							<div>
-								<label htmlFor="confirmPassword" className="sr-only">
-									Confirm Password
-								</label>
+							<label className="mt-3 block">
+								<span className="form-label">Password</span>
 								<input
-									id="confirmPassword"
-									name="confirmPassword"
 									type="password"
-									autoComplete="current-confirmPassword"
-									required
-									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-									placeholder="confirmPassword"
+									name="password"
+									className="form-input"
+									placeholder="********"
 									value={confirmPassword}
 									onChange={(e) => setConfirmPassword(e.target.value)}
 								/>
-							</div>
-						</div>
+							</label>
 
-						<div className="flex items-center justify-between">
-							<div className="flex items-center">
-								<input
-									id="remember-me"
-									name="remember-me"
-									type="checkbox"
-									className="h-4 w-4 text-violet-600 focus:ring-violet-500 border-gray-300 rounded"
-								/>
-								<label htmlFor="accept-terms" className="ml-2 block text-sm text-gray-900">
-									Agreed with the terms and condition
+							<div className="mt-4 flex items-center justify-between">
+								<label className="inline-flex items-center">
+									<input type="checkbox" name="remember" className="form-checkbox" />
+									<span className="dark--text mx-2 text-sm">Remember me</span>
 								</label>
-							</div>
-						</div>
 
-						<div>
-							<button
-								type="submit"
-								className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500">
-								<span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
-								Sign up
-								{isLoading && (
-									<div role="status">
-										<svg
-											aria-hidden="true"
-											className="inline w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-											viewBox="0 0 100 101"
-											fill="none"
-											xmlns="http://www.w3.org/2000/svg">
-											<path
-												d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-												fill="currentColor"
-											/>
-											<path
-												d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-												fill="currentFill"
-											/>
-										</svg>
-										<span className="sr-only">Loading...</span>
-									</div>
-								)}
-							</button>
-						</div>
-					</form>
+								<a
+									className="block text-sm text-primary-600 hover:underline"
+									href="/auth-forgot-password.html">
+									Forgot your password?
+								</a>
+							</div>
+
+							<div className="mt-6">
+								<button type="submit" className="btn btn-primary flex w-full" disabled={isLoading}>
+									<span className="mr-2">Sign up</span>
+									<LoadingSpinner isLoading={isLoading} />
+								</button>
+							</div>
+						</form>
+						<p className="dark--text mt-4">
+							Already have an account?
+							<Link to="/login" className="text-primary ml-2">
+								Sign in here
+							</Link>
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
