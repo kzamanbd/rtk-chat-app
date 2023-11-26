6cfb9eddc70bd2fbdb1b92c69d4beab832a3b932
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-11-26 15:38:16 +0600

    Update routes.ts

--- server/app/routes.ts
+++ server/app/routes.ts
@@ -10,7 +10,7 @@ router.get('/', async (req: Request, res: Response) => {
     res.status(200).json({
         title: 'Express Testing',
         message: 'The app is working properly!',
-        users: `${req.protocol}://${req.get('host')}${req.originalUrl}users`,
+        users: `${req.protocol}://${req.get('host')}${req.originalUrl}api/users`,
         frontend: 'https://rtk-chat-app-cyan.vercel.app'
     });
 });
